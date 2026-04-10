c326a634d4b680f34af20ba9b73399bbfe6126f3
IKEGAWA <158181191+Charlie2222223@users.noreply.github.com>
2026-04-10 16:34:37 +0900

    11章終了

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,19 +3,27 @@ class SessionsController < ApplicationController
   def new
   end
 
-  def create
-    user = User.find_by(email: params[:session][:email].downcase)
-    if user && user.authenticate(params[:session][:password])
-      forwarding_url = session[:forwarding_url]
-      reset_session
-      params[:session][:remember_me] == '1' ? remember(user) : forget(user)
+  # app/controllers/sessions_controller.rb (イメージ)
+
+def create
+  user = User.find_by(email: params[:session][:email].downcase)
+  if user && user.authenticate(params[:session][:password])
+    if user.activated?
       log_in user
-      redirect_to forwarding_url || user
+      params[:session][:remember_me] == '1' ? remember(user) : forget(user)
+      redirect_to user # ← 1回目のリダイレクト
     else
-      flash.now[:danger] = 'Invalid email/password combination'
-      render 'new', status: :unprocessable_entity
+      message  = "Account not activated. "
+      message += "Check your email for the activation link."
+      flash[:warning] = message
+      redirect_to root_url # ← 条件によってはここでもリダイレクト命令が出る！
     end
+    # ここで redirect_to などの命令が漏れて残っていませんか？
+  else
+    flash.now[:danger] = 'Invalid email/password combination'
+    render 'new'
   end
+end
 
   def destroy
     log_out if logged_in?
--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -1,31 +1,32 @@
 require "test_helper"
 
-class UsersLoginTest < ActionDispatch::IntegrationTest
+class UsersLogin < ActionDispatch::IntegrationTest
 
   def setup
     @user = users(:michael)
   end
+end
 
-  # [ログイン保持]のテスト
-  test "login with remembering" do
-    log_in_as(@user, remember_me: '1')
-    # クッキーが空でないことを確認
-    assert_not cookies[:remember_token].blank?
+class InvalidPasswordTest < UsersLogin
+
+  test "login path" do
+    get login_path
+    assert_template 'sessions/new'
   end
 
-  # [ログイン保持なし]のテスト
-  test "login without remembering" do
-    # クッキーを保存してログイン
-    log_in_as(@user, remember_me: '1')
-    # クッキーを削除してログインし直す
-    log_in_as(@user, remember_me: '0')
-    # クッキーが空になったことを確認
-    assert cookies[:remember_token].blank?
+  test "login with valid email/invalid password" do
+    post login_path, params: { session: { email:    @user.email,
+                                          password: "invalid" } }
+    assert_not is_logged_in?
+    assert_template 'sessions/new'
+    assert_not flash.empty?
+    get root_path
+    assert flash.empty?
   end
 end
 
-# 有効なログイン情報を扱うテストの「ベース」クラス
-class ValidLogin < UsersLoginTest
+class ValidLogin < UsersLogin
+
   def setup
     super
     post login_path, params: { session: { email:    @user.email,
@@ -33,11 +34,14 @@ class ValidLogin < UsersLoginTest
   end
 end
 
-# ログイン成功後の挙動を確認するテスト
 class ValidLoginTest < ValidLogin
+
   test "valid login" do
     assert is_logged_in?
     assert_redirected_to @user
+  end
+
+  test "redirect after login" do
     follow_redirect!
     assert_template 'users/show'
     assert_select "a[href=?]", login_path, count: 0
@@ -46,8 +50,8 @@ class ValidLoginTest < ValidLogin
   end
 end
 
-# ログアウト状態を前提とするテスト
 class Logout < ValidLogin
+
   def setup
     super
     delete logout_path
@@ -55,24 +59,38 @@ class Logout < ValidLogin
 end
 
 class LogoutTest < Logout
-  # 正常なログアウト
+
   test "successful logout" do
     assert_not is_logged_in?
     assert_response :see_other
     assert_redirected_to root_url
   end
 
-  # 【重要】2番目のウィンドウ（タブ）でログアウトしてもエラーにならないか
-  test "should still work after logout in second window" do
-    delete logout_path
-    assert_redirected_to root_url
-  end
-
-  # ログアウト後のリンク表示の確認
   test "redirect after logout" do
     follow_redirect!
     assert_select "a[href=?]", login_path
     assert_select "a[href=?]", logout_path,      count: 0
     assert_select "a[href=?]", user_path(@user), count: 0
+  end
+
+  test "should still work after logout in second window" do
+    delete logout_path
+    assert_redirected_to root_url
+  end
+end
+
+class RememberingTest < UsersLogin
+
+  test "login with remembering" do
+    log_in_as(@user, remember_me: '1')
+    assert_not cookies[:remember_token].blank?
+  end
+
+  test "login without remembering" do
+    # Cookieを保存してログイン
+    log_in_as(@user, remember_me: '1')
+    # Cookieが削除されていることを検証してからログイン
+    log_in_as(@user, remember_me: '0')
+    assert cookies[:remember_token].blank?
   end
 end
